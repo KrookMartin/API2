7bca669099e66d3e4a391ceaf8a01cd1961d5eb5
Martin Krook <krookmartin@yahoo.com>
2022-10-26 12:24:52 +0000

    added data expiry

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const API_KEY = "zTWzQXy7D9DPp8WJ0JWb1XuPzWQ";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
